8bfb252691cfafb2924e076c14dd59a8308142c3
Ben Dixon <ben@hillsbede.co.uk>
2014-04-13 17:57:08 +0100

    remove application.yml, we're not using it

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :keep_releases, 5
 
 # files we want symlinking to specific entries in shared
-set :linked_files, %w{config/database.yml config/application.yml}
+set :linked_files, %w{config/database.yml}
 
 # dirs we want symlinking to shared
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -29,7 +29,6 @@ set :tests, []
 # for details of operations
 set(:config_files, %w(
   nginx.conf
-  application.yml
   database.example.yml
   log_rotation
   monit
